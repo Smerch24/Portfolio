bf1f0f882d7b5648ef12c09dcea173bfb658907e
Ilia <teterailiya@gmail.com>
2024-10-05 14:35:37 +0300

    Add Page model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_10_04_112947) do
+ActiveRecord::Schema[7.0].define(version: 2024_10_05_113450) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -35,6 +35,13 @@ ActiveRecord::Schema[7.0].define(version: 2024_10_04_112947) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
+  create_table "pages", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
